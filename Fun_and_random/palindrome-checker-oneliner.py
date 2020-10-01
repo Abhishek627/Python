1f0d304c2f72856b6f09817d93cd5e3d01506a58
Vedant Khairnar <vedron007@gmail.com>
2020-10-01 18:01:53 +0530

    Description Added

--- Fun_and_random/palindrome-checker-oneliner.py
+++ Fun_and_random/palindrome-checker-oneliner.py
@@ -1,3 +1,7 @@
+'''
+A string is said to be palindrome if the reverse of the string is the same as string. For example, “madam” is a palindrome, but “abcd” is not a palindrome.
+'''
+
 def checkPalindrome(s):
     return "Palindrome" if s==s[::-1] else "Not Palindrome"
 
