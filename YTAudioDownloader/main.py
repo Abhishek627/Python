6721e732c5cdf8ef28d01ce98e89d051e1d85264
ansh rathod <32770069+anshrathod@users.noreply.github.com>
2020-10-01 18:10:30 +0530

    Update main.py

--- YTAudioDownloader/main.py
+++ YTAudioDownloader/main.py
@@ -1,4 +1,12 @@
 from __future__ import unicode_literals
+
+"""
+youtube_dl is a command-line program to download videos
+from YouTube.com. It requires the Python 2.6, 2.7, or 3.2+,
+and it is not platform specific.
+It should work on  Unix, Windows or macOS.
+"""
+
 import youtube_dl
 
 
@@ -10,6 +18,13 @@ ydl_opts = {
         'preferredquality': '192',
     }],
 }
+
+"""
+This program takes input as a list of all youtube links.
+and Downloads the audio for those youtube videos in the 
+working directory.
+""""
+
 # add all the youtube whose audio is to be downloaded links in the list below
 links = []
 for i in links:
